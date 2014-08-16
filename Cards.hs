828e1c0942a3fe2c73ab0b8b21ef8ef977129f9e
Paul <paul_roseau@yahoo.fr>
2014-08-16 18:48:35 +0200

    Added drawing random card function (not tested yet but compiles)

--- Cards.hs
+++ Cards.hs
@@ -1,11 +1,17 @@
 module Cards where
 
+import Control.Monad.State
+import System.Random
+import Data.Tuple (fst, snd, swap)
+
 data Card = Ace | Two | Three | Four |
             Five | Six | Seven | Eight |
             Nine | Ten | Jack | Queen | King
             deriving (Show)
 
 type Hand = [Card]
+
+newtype Bet = Bet (Hand, Int) deriving (Show)
 
 value :: Card -> [Int]
 value Ace = [1, 11]
@@ -22,8 +28,27 @@ value Jack = [10]
 value Queen = [10]
 value King = [10]
 
+deck :: [Card]
+deck = [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]
+
 score :: Hand -> [Int]
 score cards = foldr (\vs acc -> vs >>= (\v -> fmap (v+) acc)) [0] (map value cards)
+
+drawCard :: State StdGen Card
+drawCard = get >>= 
+           (\generator -> 
+           let (randomInt, newGenerator) = randomR (0, (lenght deck) - 1) generator
+           in put newGenerator >> return (deck !! randomInt)) 
+
+
+stand :: Bet -> Bet
+stand = id
+
+hit :: Card -> Bet -> Bet
+hit c (Bet (cs, amount)) = Bet (c:cs, amount)
+
+--double 
+
 
 -- todo
 -- "hit" is just a function which appends a new card to the player's hand
