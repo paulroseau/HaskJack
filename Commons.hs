564b1961abc9ea0b9f2d07b63e8a609927a827cb
Paul <paul_roseau@yahoo.fr>
2014-09-03 12:24:57 +0200

    Added MyWriterT monad transformer, and included it in play1round Implementation of simulate

--- Commons.hs
+++ Commons.hs
@@ -3,7 +3,6 @@ module Commons where
 -- Constants
 maxNbSplit = 4 :: Int
 blackJackQuote = 3 / 2 :: Rational
-mustHitSoftSeventeen = True
 
 -- Personal Monads
 newtype MyStateT s m a = MyStateT { runMyStateT :: s -> m (a, s) }
@@ -13,3 +12,10 @@ instance Monad m => Monad (MyStateT s m) where
   (MyStateT x) >>= f = MyStateT $ (\s ->
                                   (x s) >>= (\(a, s') -> 
                                             runMyStateT (f a) $ s'))
+
+newtype MyWriterT w m a = MyWriterT { runMyWriterT :: (m a, w) }
+
+instance Monad m, Monoid w => Monad (MyWriterT w m) where
+  return a = MyWriterT (return a, mempty)
+  (MyWriterT (x, w)) >>= f = let (y, w') = runMyWriterT (f x) 
+                             in MyWriterT (y, w `mappend` w')
--- Simulation.hs
+++ Simulation.hs
@@ -7,8 +7,17 @@ import Game
 import Strategy
 import System.Random (StdGen)
 
-play1round :: Bool -> Amount -> MyStateT (StdGen, Amount) (Either String) Amount
-play1round mustHitSoftSeventeen stake = MyStateT f 
+simulate :: Int -> Bool -> Amount -> Amount -> MyWriterT (Amount, Int) (MyStateT (StdGen, Amount) (Either String)) Amount
+simulate nbRounds mustHitSoftSeventeen refillAmount stake = foldr f (return refillAmount) [1..nbRounds]
+                                                            where f _ acc = acc >>= (\newBalance -> if newBalance < stake 
+                                                                                                      then refillBalance refillAmount 
+                                                                                                      else play1round mustHitSoftSeventeen stake)
+
+refillBalance :: Amount -> MyWriterT (Amount, Int) (MyStateT (StdGen, Amount) (Either String)) Amount 
+refillBalance refillAmount = MyWriterT (MyStateT (\(gen, balance) -> ((refillAmount, mempty), (gen, refillAmount)))
+
+play1round :: Bool -> Amount -> MyWriterT (Amount, Int) (MyStateT (StdGen, Amount) (Either String)) Amount
+play1round mustHitSoftSeventeen stake = MyWriterT (MyStateT f)
   where f (gen, initialBalance) = if (initialBalance - stake < 0) 
                                     then Left "Not enough money left to play this round!"
                                     else let (bankHand, gen1) = drawHand gen
@@ -16,28 +25,31 @@ play1round mustHitSoftSeventeen stake = MyStateT f
                                              upCard = head bankHand
                                              playerInitialStatus = Start (Bet (playerHand, stake))
                                          in if (isBlackJack bankHand)
-                                              then Right (-stake, (gen2, initialBalance - stake))
+                                              then let gains = -stake
+                                                       newBalance = initialBalance + gains                                                        
+                                                   in Right ((newBalance, (gains, 1)), (gen2, newBalance))
                                               else (runMyStateT (playBasicStrategy upCard playerInitialStatus) $ (gen2, initialBalance, 0)) >>= ( 
-                                                \(statusList, (gen3, newBalance, _)) -> let (bankScore, gen4) = runState (playBank mustHitSoftSeventeen bankHand) $ gen3
-                                                                                            gains = totalGains bankScore statusList 
-                                                                                        in gains >>= (\g -> Right (g, (gen4, newBalance + g))))
+                                                \(statusList, (gen3, updatedBalance, _)) -> let (bankScore, gen4) = runState (playBank mustHitSoftSeventeen bankHand) $ gen3
+                                                                                                gains = totalGains bankScore statusList 
+                                                                                            in gains >>= (\g -> let newBalance = updatedBalance + g 
+                                                                                                                in Right ((newBalance, (g, 1)), (gen4, newBalance))))
                                                                                       
 playBank :: Bool -> Hand -> State StdGen Int
 playBank mustHitSoftSeventeen h = if (bestScore h < 16 || mustHitSoftSeventeen && bestScore h == 17 && isSoftHand h)
-                                                    then state (\gen -> let (c, gen') = drawCard gen in (c:h, gen')) >>= playBank mustHitSoftSeventeen
-                                                    else return (bestScore h)
+                                    then state (\gen -> let (c, gen') = drawCard gen in (c:h, gen')) >>= playBank mustHitSoftSeventeen
+                                    else return (bestScore h)
 
 
 totalGains :: Int -> [Status] -> Either String Amount
 totalGains bankScore = foldr f (Right 0)
-                        where f s@(Start _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
-                              f s@(StartSplit _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
-                              f s@(StartNoSplit _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
-                              f s@(Doubled _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
-                              f s@(Continue _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
-                              f (Bust _) acc = acc
-                              f (BlackJack a) acc = acc >>= (\totalGains -> return (totalGains + (1 + blackJackQuote) * a))
-                              f (Surrender a) acc = acc >>= (\totalGains -> return (totalGains + a / 2))
-                              f (Stand (Bet (h, a))) acc = acc >>= (\totalGains -> if (bestScore h > bankScore || bankScore > 21) 
-                                                                                       then return (totalGains + 2 * a)
-                                                                                       else acc)
+                       where f s@(Start _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
+                             f s@(StartSplit _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
+                             f s@(StartNoSplit _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
+                             f s@(Doubled _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
+                             f s@(Continue _) acc = Left ("Cannot compute gains on non-final status : " ++ show s ++ ".")
+                             f (Bust _) acc = acc
+                             f (BlackJack a) acc = acc >>= (\totalGains -> return (totalGains + (1 + blackJackQuote) * a))
+                             f (Surrender a) acc = acc >>= (\totalGains -> return (totalGains + a / 2))
+                             f (Stand (Bet (h, a))) acc = acc >>= (\totalGains -> if (bestScore h > bankScore || bankScore > 21) 
+                                                                                      then return (totalGains + 2 * a)
+                                                                                      else acc)
